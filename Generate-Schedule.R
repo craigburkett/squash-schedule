e61797679d8ba0baf50dadb5bdbc67ef99b6dca3
CSBurkett <cburkett@gmail.com>
2022-08-19 11:40:00 -0400

    v2 generates games weekly using RS rankings each week, plus time since last game together

--- Generate-Schedule.R
+++ Generate-Schedule.R
@@ -1,190 +1,89 @@
-numWeeksToCreate = 5
-startDate = as.Date('2022-07-26')
+# numWeeksToCreate = 5
+# startDate = as.Date('2022-07-26')
+maxDaysLimit = 90
 
 library(tidyverse)
-# library(rvest)
+library(magrittr)
 library(jsonlite)
-library(TouRnament)
+# library(rvest)
+# library(TouRnament)
+
+# source("Helpers.R")
+
 options(dplyr.summarise.inform=F)
 
 outDir = "Output/"
+if(!dir.exists(outDir)) dir.create(outDir)
+
+todaysDate = Sys.Date()
 
 #### DATA PREP ####
 
-## Simple input file
-raw = read.csv("Squash-Input-RA-TNCL-2022-Summer.csv")
+## Names and Ranks from Rankenstein directly
+url <- "https://rankenstein.ca/api.pl?action=rankings" # &club=RA
+rankings <- url %>% fromJSON(flatten = T) %>% extract2("rankings") %>% select(starts_with("player"), rating) %>% rename(Name = player.name, ID = player.id)
 
-inNames = raw %>%
-  rename(Level = "X") %>%
-  pivot_longer(starts_with("X"), names_to = "Team", values_to = "Player") %>%
-  mutate(Team = sub("X", "", Team)) %>%
-  filter(Player != "")
+## Simple input file - join to ratings online
+players = read.csv("Squash-Input-RA-TNCL-2022-Fall.csv") %>%
+  left_join(rankings, by = "Name") %>%
+  mutate(rating = ifelse(is.na(rating), RatingEstimate, rating)) %>%
+  select(Name, rating) %>%
+  arrange(Name)
 
-## From Rankenstein directly
-url <- "https://rankenstein.ca/api.pl?action=rankings&club=RA"
-table <- url %>% fromJSON() 
-
-# TODO: Get rankings and make levels
-
-#### FUNCTIONS ####
-
-roundrobin_custom <- function(dfr, numWeeks) {
-  generate_schedule(dfr, numWeeks) %>%
-    pivot_longer(starts_with("Week"), names_to = "Matchday", values_to = "GameID") %>%
-    mutate(Matchday = as.numeric(sub("Week", "", Matchday))) %>%
-    group_by(Matchday, GameID) %>%
-    summarize(
-      Home = first(Player)
-      , Away = last(Player)
-    ) %>% 
-    select(-GameID)
-} 
-
-is_week_bad <- function(inNames) {
-  inNames %>%
-    group_by(GameID) %>%
-    summarize(
-      EqualLevel = all(Level == first(Level)) # TODO: Faster aggregation function
-      , DifferentTeam = !all(Team == first(Team))
-    ) %>%
-    summarize(
-      allLevelsGood = all(EqualLevel)
-      , allTeamsGood = all(DifferentTeam)
-    ) %>%
-    mutate(AnyBad = !(allLevelsGood & allTeamsGood)) %>%
-    pull(AnyBad)
-}
-
-is_schedule_bad <- function(dfr) {
-
-  players = dfr$Player
-  isBad = FALSE
-
-  # for(i in 1:numWeeks) dfr[i + nCols] = generate_week(inNames)
-  long = dfr %>%
-    pivot_longer(cols = starts_with("Week"), names_to = "Week", values_to = "ID") %>%
-    mutate(Week = sub("Week", "", Week))
-
-  for(pl in players){
-    numInLevel = dfr %>% filter(Level == dfr$Level[dfr$Player == pl]) %>% nrow()
-
-    opponents = long %>%
-      filter(Player == pl) %>%
-      select(Week, ID) %>%
-      inner_join(long, by = c("Week", "ID")) %>%
-      filter(Player != pl) %>%
-      group_by(Player) %>%
-      tally()
-
-    if(nrow(opponents) < (numInLevel - 1)){
-      cat("\nSchedule does not afford each player a game against all opponents in level", dfr$Level[dfr$Player == pl])
-      isBad = TRUE
-      break
-    }
-
-    if(max(opponents$n) > min(opponents$n) + 1){
-      cat("\nSchedule allocates uneven game counts in level", dfr$Level[dfr$Player == pl])
-      isBad = TRUE
-      break
-    }
-  }
-  isBad
-}
-
-generate_week <- function(inNames) {
-  # Level must match
-  # Team cannot match
-  # Only one game (per week)
-
-  ## TODO: Does not work for odd-numbered players in a level (as one would have to play someone from another level)
-
-  nGames = if(nrow(inNames) %% 2 == 0) nrow(inNames)/2 else (nrow(inNames) - 1 / 2)
-  gameIDs = rep(1:nGames, each=2)
-  isBad = TRUE
-  numLevels = length(unique(inNames$Level))
-  levelNames = unique(inNames$Level)
-
-  inNames$GameID = NA # Create space so not done within loop
-
-  while(isBad){
-    for(lev in levelNames){
-      inNames$GameID[inNames$Level == lev] = sample(gameIDs[which(inNames$Level == lev)])
-    }
-
-    isBad = is_week_bad(inNames)
-  }
-
-  inNames$GameID
-}
-
-generate_schedule <- function(dfr, numWeeks) {
-  # Should play everyone if possible
-  # Shouldn't play N+2 games against one opponent if only N games against another
-  # numWeeks = 9
-
-  nCols = ncol(dfr) # Info columns coming in, could change in future
-
-  ## Create new column for each week
-  dfrWide = data.frame(
-    Week = paste0("Week", 1:numWeeks)
-    ,Value = NA
-  ) %>%
-    pivot_wider(names_from = Week, values_from = Value) %>%
-    cbind(dfr, .)
-  isBad = TRUE
-
-  while(isBad){
-    ## Generate a random schedule for all weeks
-    for(i in 1:numWeeks) dfrWide[i + nCols] = generate_week(dfrWide)
-
-    isBad = is_schedule_bad(dfrWide)
-  }
-  
-  dfrWide
-}
-
-make_schedule_for_level <- function(dfr, numWeeks = 5) {
-  ## Will only do two full cycles at a time 
-  # dfr = dd[[1]]
-  # dfr = dd[[5]]
-  # numWeeks = numWeeksToCreate
-  
-  # n = nrow(dfr)
-  # combs = choose(n, 2)
-  
-  thisLevel = dfr %>% pull(Level) %>% unique()
-  stopifnot(length(thisLevel) == 1)
-  
-  ## Add extra players in case there are less than 5 (required for roundrobin function to work properly)
-  # while(nrow(dfr) < 6) dfr = dfr %>% add_row(Level = "E", Player = letters[nrow(dfr) + 1])
-  if(nrow(dfr) < 6){
-    res = roundrobin_custom(dfr, numWeeks) %>% mutate(Level = thisLevel)
-  } else {
-    res = dfr %>% 
-      pull(Player) %>%
-      roundrobin(second_round=T,match_free=TRUE,randomize=TRUE) %>%
-      filter(Matchday <= numWeeks) %>%
-      mutate(Level = thisLevel)
-  }
-  
-  res
-}
+# TODO: Some kind of error checking on input names, if I can't find them in Rankenstein
 
 #### MAINLINE ####
+ourPlayers = pull(players, Name)
 
-DaysDF = data.frame(
-  MatchDate = seq(startDate, by = 7, length.out = numWeeksToCreate)
-  , Matchday = 1:numWeeksToCreate
-)
+## Get previous played games and date of match
+url <- "https://rankenstein.ca/api.pl?action=results" # &club=RA
+results <- url %>% 
+  fromJSON(flatten = T) %>% 
+  select(loserName, winnerName, winnerId, loserId, date) %>% # About 15s to pull
+  filter(loserName %in% ourPlayers & winnerName %in% ourPlayers)
 
-scheduleDF = inNames %>% 
-  group_by(Level) %>% 
-  group_split() %>%
-  lapply(make_schedule_for_level, numWeeks = numWeeksToCreate) %>%
-  reduce(bind_rows) %>%
-  select(Level, Matchday, Home, Away) %>%
-  inner_join(DaysDF, by = "Matchday") %>%
-  select(-Matchday) %>%
-  arrange(MatchDate)
+## Each pairing and the number of days since their last match
+summ = results %>%
+  mutate(
+     PlayerA = ifelse(winnerName < loserName, winnerName, loserName)
+    ,PlayerB = ifelse(winnerName > loserName, winnerName, loserName)
+    ) %>%
+  select(PlayerA, PlayerB, date) %>%
+  distinct() %>%
+  group_by(PlayerA, PlayerB) %>%
+  summarize(DaysSinceLastGame = min(todaysDate - as.Date(date)))
 
-write_csv(scheduleDF, paste0(outDir, "Squash-Schedule-RA-TNCL-2022-Summer-Starting-", startDate, ".csv"))
+fullSumm = summ %>%
+  rename(Opponent = PlayerB, Player = PlayerA) %>%
+  select(Player, Opponent, DaysSinceLastGame) %>%
+  bind_rows(rename(summ, Opponent = PlayerA, Player = PlayerB))
+
+allPairings = expand.grid(Opponent = ourPlayers, Player = ourPlayers) %>%
+  left_join(fullSumm, by = c("Player", "Opponent")) %>%
+  left_join(players, by = c("Player" = "Name")) %>%
+  rename(PlayerRating = rating) %>%
+  left_join(players, by = c("Opponent" = "Name")) %>%
+  rename(OpponentRating = rating) %>%
+  mutate(RatingDiff = abs(PlayerRating - OpponentRating)) %>%
+  filter(Player != Opponent) %>%
+  replace_na(list(DaysSinceLastGame = maxDaysLimit)) %>%
+  # TODO: Opponents since last game instead? Not symmetrical though ...
+  mutate(DaysSinceLastGame = ifelse(DaysSinceLastGame > maxDaysLimit, maxDaysLimit, DaysSinceLastGame)) %>%
+  # TODO: Get court time preferences and consider in scoring function
+  mutate(Similarity = get_similarity(RatingDiff, DaysSinceLastGame))
+
+## Loop through players starting from the top, and take the best opponent one at a time
+# TODO: Consider all players simultaneously
+workingPairings = allPairings
+loopPlayers = allPairings %>% select(Player, PlayerRating) %>% distinct() %>% arrange(desc(PlayerRating)) %>% pull(Player)
+while(length(loopPlayers) > 1){
+  player = loopPlayers[1]
+  opponent = workingPairings %>% filter(Player == player) %>% filter(Similarity == min(Similarity)) %>% pull(Opponent)
+  cat(paste0("\nMatch created: ", player, " (", players$rating[players$Name == player],") vs ", opponent, " (", players$rating[players$Name == opponent],")"))
+  loopPlayers = setdiff(loopPlayers, c(player, opponent))
+  workingPairings = filter(workingPairings, Opponent != opponent, Opponent != player)
+}
+
+
+
+# write_csv(scheduleDF, paste0(outDir, "Squash-Schedule-RA-TNCL-2022-Summer-Starting-", startDate, ".csv"))
