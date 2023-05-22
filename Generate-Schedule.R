992c07b9a717cf84af10efb2defd08635e417772
CSBurkett <cburkett@gmail.com>
2023-05-22 12:04:28 -0400

    Modified to work with WSQ league

--- Generate-Schedule.R
+++ Generate-Schedule.R
@@ -27,6 +27,9 @@ data.frame(Home=character(), HomeRating=integer(), Away=character(), AwayRating=
 
 ## Names and Ranks from Rankenstein directly
 url <- "https://rankenstein.ca/api.pl?action=rankings" # &club=RA
+# url <- "https://rankenstein.ca/api.pl?action=rankings&club=WSQ"
+# url <- "https://rankenstein.ca/api.pl?action=rankings&club=WSQ&status=INACTIVE"
+
 rankings <- url %>% fromJSON(flatten = T) %>% extract2("rankings") %>% select(starts_with("player"), rating) %>% rename(Name = player.name, ID = player.id)
 
 ## Simple input file - join to ratings online
@@ -41,6 +44,8 @@ players = read.csv(file.choose()) %>%
 tt = players %>% filter(is.na(ID))
 message("Names not found in Rankenstein:\n\n")
 message(paste0(capture.output(tt), collapse = '\n'))
+
+# players %>% select(Name, Division, rating) %>% arrange(Division, desc(rating))
 
 players = players %>%
   select(Name, rating) %>%
@@ -66,7 +71,7 @@ summ = results %>%
   select(PlayerA, PlayerB, date) %>%
   distinct() %>%
   group_by(PlayerA, PlayerB) %>%
-  summarize(DaysSinceLastGame = min(todaysDate - as.Date(date)))
+  summarize(DaysSinceLastGame = min(todaysDate - as.Date(date)) %>% as.numeric())
 
 fullSumm = summ %>%
   rename(Opponent = PlayerB, Player = PlayerA) %>%
@@ -125,3 +130,30 @@ while(length(loopPlayers) > 0){
   workingPairings = filter(workingPairings, Opponent != opponent, Opponent != player)
 }
 # TODO: Last two players might have just played, should look back and swap if this is the case
+# 
+# Match created: Assad Khan (8198) 	        vs Jason Salares (7985) 	          - 90 or more days since last match
+# Match created: Andrew Papp-Castari (7986)	vs Walter Dicesare (7783) 	        - 90 or more days since last match
+# Match created: Toby Kemp (7955) 	        vs Tracey Sly (7707) 	              - 90 or more days since last match
+# Match created: Joe Gallant (7697) 	      vs Jason Kim (7595) 	              - 90 or more days since last match
+# Match created: Rahmat Shahidi (7579) 	    vs Nick Zhang (7540) 	              - 90 or more days since last match
+# Match created: Julie MacElwee (7499) 	    vs Sharat Gupta (7493) 	            - 90 or more days since last match
+# Match created: Helen Kennedy (7494) 	    vs Hassan Sarbishaei (7489) 	      - 90 or more days since last match
+# Match created: Lorraine Tetreault (7439) 	vs Clive Dsousa (7370) 	            - 90 or more days since last match
+# Match created: Lisa MacElwee (7402) 	    vs James Harley (7296) 	            - 90 or more days since last match
+# Match created: Sam Goolamallee (7364) 	  vs Dan Chow (7280) 	                - 90 or more days since last match
+# Match created: Lyndsay Gardiner (7363) 	  vs Andre Lamprecht (7311) 	        - 90 or more days since last match
+# Match created: Brent Powell (7273) 	      vs Matt Delaurier (7124) 	          - 90 or more days since last match
+# Match created: Bernard St.Denis (7266) 	  vs Richard Swift (7006) 	          - 90 or more days since last match
+# Match created: Craig Burkett (6754) 	    vs Kevin Gray (6692) 	              - 90 or more days since last match
+# Match created: Shawn Dawson (6669) 	      vs Detlef Dransch (6606) 	          - 90 or more days since last match
+# Match created: Ammar Gupta (6500) 	      vs Tom Montor (6498) 	              - 90 or more days since last match
+# Match created: Anthony Wright (6491) 	    vs Paula Piilonen (6309) 	          - 90 or more days since last match
+# Match created: Matt Taylor (6456) 	      vs Steven Charlebois-Page (6303) 	  - 90 or more days since last match
+# Match created: Francis Mcquaid (6300) 	  vs Ray Nazarzai (6288) 	            - 90 or more days since last match
+# Match created: Aadi Dua (6284) 	          vs Richard Katan (6272) 	          - 90 or more days since last match
+# Match created: Doug Herbert (6245) 	      vs Syed Hassan (6218) 	            - 90 or more days since last match
+# Match created: Tim Ambery (6235) 	        vs Peter Crawley (6232) 	          - 90 or more days since last match
+# Match created: Eric Mia (6200) 	          vs Ben Tomka (6187) 	              - 90 or more days since last match
+# Match created: Shreyans Shah (6200) 	    vs <SPARE>
+
+
--- Helpers.R
+++ Helpers.R
@@ -1,4 +1,18 @@
 
 get_similarity <- function(RatingDiff, DaysSinceLastGame) {
+  
   RatingDiff + (maxDaysLimit - DaysSinceLastGame)^1.4
+}
+
+expand.grid.unique <- function(x, y, include.equals=FALSE){
+  x <- unique(x)
+  y <- unique(y)
+  
+  g <- function(i)
+  {
+    z <- setdiff(y, x[seq_len(i-include.equals)])
+    if(length(z)) cbind(x[i], z, deparse.level=0)
+  }
+  
+  do.call(rbind, lapply(seq_along(x), g))
 }
